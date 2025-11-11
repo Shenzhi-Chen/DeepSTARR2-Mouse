9b4ed5a5166de489d0931c1fc237c4f9d83d1982
Shenzhi Chen <167293516+Shenzhi-Chen@users.noreply.github.com>
2025-11-11 11:30:09 +0100

    Enhance model training script with job ID handling
    Added job ID retrieval and prediction logging for enhancer activity model training.

--- Enhancer_activity_model_training/Run_models.sh
+++ Enhancer_activity_model_training/Run_models.sh
@@ -21,6 +21,10 @@ for fold in ${fold_list//,/ }; do
 											 -v class \
 											 -a ${arch}/Model \
 											 -o ${OUTDIR}/Model" > ${OUTDIR}/log_training/msg.model_training.tmp
+											 
+		# get job IDs to wait for mapping to finish
+		ID_main=$(paste ${OUTDIR}/log_training/msg.model_training.tmp | grep Submitted | awk '{print $4}')		
+		
 		# Predict enhancer test dataset
 		mkdir -p ${OUTDIR}/log_predictions
 		JOB_ID=act_VISTA_pred_${ID}_init_random
@@ -28,6 +32,7 @@ for fold in ${fold_list//,/ }; do
 											 -o ${OUTDIR}/log_predictions \
 											 -m 80 \
 											 -T '4:00:00' \
+											 -d "${ID_main}" \
 											 "${script_path}/Predict_CNN_model_from_fasta.py \
 											 -s ${InputID}/${tissue}/${fold}_sequences_test.fa \
 											 -m ${OUTDIR}/Model \
@@ -38,6 +43,7 @@ for fold in ${fold_list//,/ }; do
 											 -o ${OUTDIR}/log_predictions \
 											 -m 80 \
 											 -T '4:00:00' \
+											 -d "${ID_main}" \
 											 "${script_path}/Predict_CNN_model_from_fasta.py \
 											 -s testing_dataset/random_sequences_600k.fasta \
 											 -m ${OUTDIR}/Model \
@@ -47,6 +53,7 @@ for fold in ${fold_list//,/ }; do
          scripts/functions/my_bsub_gridengine -m 60 \
                                        -T '10:00:00' \
                                        -P g -G "gpu:3" \
+									   -d "${ID_main}" \
                                        -o ${OUTDIR}/${tissue}/results_${fold}_${tissue}_${arch}_rep${rep}/log_explainer \
                                        -n ${tissue}/${fold}_DeepExplainer \
                                        "Accessibility_model_training/run_DeepSHAP_DeepExplainer.py \
@@ -57,6 +64,35 @@ for fold in ${fold_list//,/ }; do
                                        -s ${fold}_sequences_test.fa \
                                        -w 1001 \
                                        -b dinuc_shuffle" > ${OUTDIR}/log_explainer/msg.contr_scores_${fold}.tmp
+
+		# directly training activity model (work as conrtol)
+        OUTDIR=result/model/enhancer_activity_model/${tissue}/results_${fold}_${tissue}_DeepSTARR_rep${rep}_init_random
+		mkdir -p ${OUTDIR}
+		mkdir -p ${OUTDIR}/log_training
+		scripts/functions/my_bsub_gridengine  -P g \
+											  -G "gpu:1" \
+											  -m 60 \
+											  -T '2:00:00' \
+											  -o ${OUTDIR}/log_training \
+											  -n act_Training_init_random \
+											  "${script_path}/Train_random_initi_model.py  \
+											  -i ${InputID}/${tissue}/${fold} \
+											  -v class \
+											  -o ${OUTDIR}/Model" > ${OUTDIR}/log_training/msg.model_training.tmp
+		# get job IDs to wait for mapping to finish
+		ID_main=$(paste ${OUTDIR}/log_training/msg.model_training.tmp | grep Submitted | awk '{print $4}')
+		# Predict enhancer test dataset
+		mkdir -p ${OUTDIR}/log_prediction
+		JOB_ID=act_VISTA_pred_${ID}_init_random
+		scripts/functions/my_bsub_gridengine -n ${JOB_ID} \
+											 -o ${OUTDIR}/log_predictions \
+											 -m 80 \
+											 -d "${ID_main}" \
+											 -T '4:00:00' \
+											 "${script_path}/Predict_CNN_model_from_fasta.py \
+											 -s ${InputID}/${tissue}/${fold}_sequences_test.fa \
+											 -m ${OUTDIR}/Model \
+											 -o ${OUTDIR}"   
   	    done
     done
 done
